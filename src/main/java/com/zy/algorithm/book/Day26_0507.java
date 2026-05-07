c562443c09f5fa40677cecc4ec4ee5a2f4641634
zhaozuyuan <zuyuan1024@foxmail.com>
2026-05-08 00:33:22 +0800

    day26_0507 大数乘积字符串

---
+++ src/main/java/com/zy/algorithm/book/Day26_0507.java
@@ -0,0 +1,44 @@
+package com.zy.algorithm.book;
+
+public class Day26_0507 {
+    public static void main(String[] args) {
+        assert bigNumberMultiplication("11", "22").equals("22");
+    }
+
+    /**
+     * 大数相乘
+     * @param num1 高位数在前
+     * @param num2
+     * @return 乘积，高位数在前
+     */
+    public static String bigNumberMultiplication(String num1, String num2) {
+        // 结果，低位数在前
+        int[] result = new int[num1.length() + num2.length()];
+        for (int i1 = num1.length() - 1; i1 >= 0; --i1) {
+            for (int i2 = num2.length() - 1; i2 >= 0; --i2) {
+                int product = (num1.charAt(i1) - '0') * (num2.charAt(i2) - '0');
+
+                int currentIndex = result.length - (i1 + i2) - 2;
+                int sum = result[currentIndex] + product;
+                result[currentIndex] = sum % 10;
+                while (sum >= 10){
+                   ++currentIndex;
+                   sum = result[currentIndex] + sum / 10;
+                   result[currentIndex] = sum % 10;
+                }
+            }
+        }
+
+        // 转换成高位数在前
+        for (int i = result.length - 1; i >= 0; --i) {
+            if(result[i] != 0) {
+                StringBuilder builder = new StringBuilder();
+                for (int j = i; j >= 0; --j) {
+                    builder.append(result[j]);
+                }
+                return builder.toString();
+            }
+        }
+        return "0";
+    }
+}
