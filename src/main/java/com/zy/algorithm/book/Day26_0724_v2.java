f0f3b1a5db4baa160abc3c357613d169c403120d
zhaozuyuan <zuyuan1024@foxmail.com>
2026-08-11 15:51:53 +0800

    docs: 补齐Day26_0724_v2.java Day26_0729.java注释

--- src/main/java/com/zy/algorithm/book/Day26_0724_v2.java
+++ src/main/java/com/zy/algorithm/book/Day26_0724_v2.java
@@ -7,7 +7,7 @@ public class Day26_0724_v2 {
     /**
      * 题目描述：
      *
-     * 给定一个候选人编号的集合 candidates 和一个目标数 target，找出 candidates 中所有可以使数字和为 target 的组合。
+     * leetcode.39 给定一个候选人编号的集合 candidates 和一个目标数 target，找出 candidates 中所有可以使数字和为 target 的组合。
      * candidates 中的每个数字在每个组合中只能使用一次。
      *
      * 注意：解集不能包含重复的组合。
